f307295288c1ccbb8684a509c03ebb81fe955948
DipaWidia <wiartha2001@gmail.com>
2022-11-26 10:16:34 +0800

    astungkara mau

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from app import app
-from app.controller import UserController, ChatbotController
+from app.controller import UserController
 
 
 @app.route('/')
