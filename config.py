13340fd6e4808feeab4260cc8e82c2f03a3da86b
DipaWidia <wiartha2001@gmail.com>
2022-11-26 11:22:13 +0800

    astungkara mau v4

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     PASSWORD = str(os.environ.get("DB_PASSWORD"))
     
     JWT_SECRET_KEY = str(os.environ.get("JWT_SECRET"))
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user1:Sr9%TU}Q,*dO89?Z@34.101.214.166:3306/chatbot_deeplearning'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://wfcyqpqhpqpgth0z:qzfai2cyr2sly2ul@x71wqc4m22j8e3ql.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vnm9v4jty1f321fh'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     
