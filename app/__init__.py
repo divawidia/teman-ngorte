68196afa10df54c1072abb0ebbe38b100c42fd81
DipaWidia <wiartha2001@gmail.com>
2022-11-26 14:58:00 +0800

    p

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
+import os
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -15,4 +17,4 @@ from app.model import user, message
 from app import routes
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
